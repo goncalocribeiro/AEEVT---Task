15b72681e946082dfb7e3074d36679edf1b34bdb
Gonçalo Ribeiro <goncalocribeiro@tecnico.ulisboa.pt>
2018-05-10 21:50:23 +0200

    deJongFirst (stil to test)

--- deJongFirst.py
+++ deJongFirst.py
@@ -1,8 +1,10 @@
 #GONCALO RIBEIRO - goncalocribeiro@tecnico.ulisboa.pt
 #AEEVT - First DeJong Function
 
-function cost = deJongFirst(xx)
-d = length(xx)
-sum = 0
-for i in range(1,d):
-	 xi = xx[i]
+def deJongFirst(xx):
+	d = length(xx)
+	sum = 0
+	for i in range(1,d):
+		xi = xx[i]
+		sum = sum + xi^2
+	return sum
